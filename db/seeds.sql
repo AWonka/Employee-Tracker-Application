6ce46edfd6d32fb3e2eada41b70630a38c27b8ef
Austin Wonka <wonkaaustin@gmail.com>
2022-01-24 18:38:31 -0500

    redid the seeds to be better formatted.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,17 @@
-INSERT INTO department (id, name)
-VALUES (1, "Sales"),
-       (2, "Accounting"),
-       (3, "Design");
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Accounting"),
+       ("Design");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (001, "Sales Associate", 50000.00, 1),
-       (002, "Assitant Accountant", 45000.00, 2),
-       (003, "Graphics Designer", 48000.00, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Associate", 50000.00, 1),
+       ("Assitant Accountant", 45000.00, 2),
+       ("Graphics Designer", 48000.00, 3);
 
-INSERT INTO  employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Chris", "Christenson", 001, NULL),
-       (2, "John", "Jonnyson", 002, 1),
-       (3, "Sadie", "McLadie", 003, 2); 
+INSERT INTO  employee (first_name, last_name, role_id, manager_id)
+VALUES ("Chris", "Christenson", 1, NULL),
+       ("John", "Jonnyson", 1, 1),
+       ("Sadie", "McLadie", 2, NULL),
+       ("Adam", "Adamson", 2, 3),
+       ("Rich", "Richardson", 3, NULL),
+       ("Sarah", "Sarahson", 3, 5);
