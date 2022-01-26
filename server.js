194c4300f172885ab71b8b2cc1dfe6ee9ec1a3bb
Austin Wonka <wonkaaustin@gmail.com>
2022-01-26 16:06:47 -0500

    added switch case for choices

--- server.js
+++ server.js
@@ -29,4 +29,32 @@ const promptUser = () => {
             choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
         }
     ])
+    .then((answer) => {
+        switch(answer.choices) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployee();
+                break;
+            default:
+                console.log('Please select an option');
+                promptUser();                            
+        }
+    });
 };
